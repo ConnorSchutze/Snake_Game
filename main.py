e4c5ea47a7d3213d43e243118fa18f572e1b98cf
ConnorSchutze <connorschutze@gmail.com>
2022-04-18 23:23:15 -0700

    Created a simple plan.

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
    Description: Executes all of the code and displays it on screen.
 """
 
-# Pushing this
+# main.py -> 1 -> 2
+# 1. main menu - tkinter (menu.py)
+   # tkinter function (paramter = highscore)
+   # tkinter window run
+   # tkinter window close
+# 2. snake game - pygame (snake.py, food.py, score.py)
 
 # Directory ->(Adding) # Staging Area ->(Committing) # Git ->(Push) -> Github
