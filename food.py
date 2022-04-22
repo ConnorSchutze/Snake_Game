c82a0a2461ef177a158463ea67114d57c661aed3
ConnorSchutze <connorschutze@gmail.com>
2022-04-22 14:51:26 -0700

    food.py styles fixed.

--- food.py
+++ food.py
@@ -1,12 +1,22 @@
+"""File: food.py.
+
+Team NO: 5
+Author: Connor Schutze
+Description: Generating food onto the screen in random places.
+"""
+
 import pygame
 import random
 from pygame.math import Vector2
 
 
 class Food:
-    """Creating a food object for the snake to eat."""
+    """Create a food object for the snake to eat."""
+
     def __init__(self, cell_size, cell_width, cell_height):
-        """Get the amount of cells, sizes, and display surface.
+        """Create the food attributes.
+
+        Get the amount of cells, sizes, and display surface.
         As well as set the position of the original food placement.
         """
         pygame.init()
@@ -21,13 +31,14 @@ class Food:
 
     def draw(self):
         """Draw the fruit onto the display surface."""
-        food_rect = pygame.Rect(int(self.position.x * self.cell_size), 
-                                int(self.position.y * self.cell_size), 
+        food_rect = pygame.Rect(int(self.position.x * self.cell_size),
+                                int(self.position.y * self.cell_size),
                                 self.cell_size, self.cell_size)
         self.screen.blit(self.apple, food_rect)
-    
+
     def random_position(self):
         """Find one random cell on screen for the food to spawn."""
         self.x = random.randint(0, self.cell_width-1)
-        self.y = random.randint(self.cell_width-self.cell_height, self.cell_height-1)
+        self.y = random.randint(self.cell_width-self.cell_height,
+                                self.cell_height-1)
         self.position = Vector2(self.x, self.y)
